9be35ab710d8ce9b0a2740c1dc2e5b710575c99e
Vikas Dhiman <wecacuee@users.noreply.github.com>
2019-06-15 12:56:51 -0700

    Backward imcompatible shuffling.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(name='kwplus',
       description='Keyword arguments on steroids',
       author='Vikas Dhiman',
       url='https://github.com/wecacuee/kwplus',
-      author_email='dhiman@umich.edu',
-      version='0.3.4',
+      author_email='wecacuee@github.com',
+      version='1.0.0',
       license='MIT',
       classifiers=(
           'Development Status :: 3 - Alpha',
@@ -15,4 +15,4 @@ setup(name='kwplus',
           "Operating System :: OS Independent",
       ),
       python_requires='>=3.5',
-      py_modules=['kwplus', 'kwrepr', 'kwvarations'])
+      packages=find_packages())
